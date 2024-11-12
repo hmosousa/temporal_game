e8f314b1d5893c985dc2dd41497368fff1e59f39
Hugo Sousa <hmosousa@gmail.com>
2024-11-12 16:24:35 +0000

    Organize results folder

--- scripts/classifier/run.sh
+++ scripts/classifier/run.sh
@@ -10,8 +10,13 @@ accelerate launch --config_file configs/accelerate/zero2.yaml scripts/classifier
 accelerate launch --config_file configs/accelerate/zero2.yaml scripts/classifier/train.py -c classifier/smol_balanced.yaml
 accelerate launch --config_file configs/accelerate/zero2.yaml scripts/classifier/train.py -c classifier/smol.yaml
 
+CUDA_VISIBLE_DEVICES=3 python scripts/classifier/train.py -c classifier/smol.yaml
+
 # Evaluate the classifiers
 CUDA_VISIBLE_DEVICES=3 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_b -d timeset
 CUDA_VISIBLE_DEVICES=3 python scripts/classifier/eval.py -m hugosousa/classifier_llama_3b_balanced -d q_timelines
 
-python scripts/classifier/train.py -c classifier/smol.yaml
+
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m -d q_timelines
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m -d timeset
+
