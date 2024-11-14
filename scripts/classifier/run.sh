adf85785d8fc5b58a5f05373d661ed5fcc58f6b4
Hugo Sousa <hmosousa@gmail.com>
2024-11-14 14:16:14 +0000

    Add resutls

--- scripts/classifier/run.sh
+++ scripts/classifier/run.sh
@@ -13,10 +13,18 @@ accelerate launch --config_file configs/accelerate/zero2.yaml scripts/classifier
 CUDA_VISIBLE_DEVICES=3 python scripts/classifier/train.py -c classifier/smol.yaml
 
 # Evaluate the classifiers
-CUDA_VISIBLE_DEVICES=3 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_b -d timeset
-CUDA_VISIBLE_DEVICES=3 python scripts/classifier/eval.py -m hugosousa/classifier_llama_3b_balanced -d q_timelines
+CUDA_VISIBLE_DEVICES=0 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_b -d timeset
+CUDA_VISIBLE_DEVICES=0 python scripts/classifier/eval.py -m hugosousa/classifier_llama_3b_balanced -d q_timelines
 
 
-CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m -d q_timelines
-CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m -d timeset
+CUDA_VISIBLE_DEVICES=0 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_20241113151941 -d q_timelines
+CUDA_VISIBLE_DEVICES=0 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_20241113151941 -d timeset
 
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_augmented_20241113152434 -d q_timelines
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_augmented_20241113152434 -d timeset
+
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_b_20241113152343 -d q_timelines
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_b_20241113152343 -d timeset
+
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_b_a_20241113152527 -d q_timelines
+CUDA_VISIBLE_DEVICES=1 python scripts/classifier/eval.py -m hugosousa/classifier_smoll_135m_b_a_20241113152527 -d timeset
