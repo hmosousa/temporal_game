2ef7de09348d52ec4d574cef28278f826ea1215d
Hugo Sousa <hmosousa@gmail.com>
2024-12-20 13:18:34 +0000

    Drop last checkpoint from trainning script

--- scripts/train_hf.py
+++ scripts/train_hf.py
@@ -12,6 +12,7 @@ import transformers
 from datasets import load_dataset
 
 from src.constants import HF_TOKEN
+
 from transformers import (
     AutoConfig,
     AutoModelForSequenceClassification,
@@ -288,27 +289,6 @@ def main(
     )
     logger.info(f"Training/evaluation parameters {training_args}")
 
-    # Detecting last checkpoint.
-    last_checkpoint = None
-    # if (
-    #     os.path.isdir(training_args.output_dir)
-    #     and training_args.do_train
-    #     and not training_args.overwrite_output_dir
-    # ):
-    #     last_checkpoint = get_last_checkpoint(training_args.output_dir)
-    #     if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
-    #         raise ValueError(
-    #             f"Output directory ({training_args.output_dir}) already exists and is not empty. "
-    #             "Use --overwrite_output_dir to overcome."
-    #         )
-    #     elif (
-    #         last_checkpoint is not None and training_args.resume_from_checkpoint is None
-    #     ):
-    #         logger.info(
-    #             f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
-    #             "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
-    #         )
-
     # Set seed before initializing model.
     set_seed(training_args.seed)
 
@@ -498,12 +478,7 @@ def main(
 
     # Training
     if training_args.do_train:
-        checkpoint = None
-        if training_args.resume_from_checkpoint is not None:
-            checkpoint = training_args.resume_from_checkpoint
-        elif last_checkpoint is not None:
-            checkpoint = last_checkpoint
-        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        train_result = trainer.train()
         metrics = train_result.metrics
         max_train_samples = (
             data_args.max_train_samples
